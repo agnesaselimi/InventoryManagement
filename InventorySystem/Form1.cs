57cd5c5d8e96b1b33036dc14e3a0d2836e2687d8
Agnesa Selimi <agnesa.selimi@gmail.com>
2018-03-17 00:51:41 +0100

    first commit

---
+++ InventorySystem/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InventorySystem
+{
+    public partial class FormLogin : Form
+    {
+        private class LoginImpl : LoginInterface
+        {
+    
+            public void onUserFailToLogin(string message)
+            {
+                MessageBox.Show(message);
+            }
+
+            public void onUserLogin()
+            {
+                MainForm MF = new MainForm();
+                MF.Show();
+
+            }
+        }
+
+        private LoginImpl loginImpl;
+
+        public FormLogin()
+        {
+            InitializeComponent();
+            loginImpl = new LoginImpl();
+        }
+
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+           
+            
+            string Username = tbUserName.Text;
+            string Password = tbPassword.Text;
+            if (Username == "" && Password == "")
+            {
+                this.Hide();
+                loginImpl.onUserLogin();
+
+            }
+            else
+            {
+                loginImpl.onUserFailToLogin("User not found.");
+            }
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ InventorySystem/ProductDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InventorySystem
+{
+    public partial class ProductDetail : Form
+    {
+        public ProductDetail()
+        {
+            InitializeComponent();
+        }
+
+        private void ProductDetail_Load(object sender, EventArgs e)
+        {
+            DataTable dt = ClassInventory.Instance.ProductTable();
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
